3e90ec660295b50ee309f754f12ece8cb6e300a3
Spyros Seimenis <spyros.seimenis@canonical.com>
2024-07-25 02:02:48 +0300

    i/fs/fs.go: add new fs helper for retrieving filesystem block count

---
+++ internal/fs/fs.go
@@ -0,0 +1,43 @@
+package fs
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+
+	internal_exec "github.com/canonical/encrypt-cloud-image/internal/exec"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	BlockSize = 4096
+)
+
+var (
+	blockCountPatternRe = regexp.MustCompile(`Block count: *([0-9]*)`)
+)
+
+func GetBlockCount(devPath string) (uint64, error) {
+	log.Infoln("calculate filesystem block count on", devPath)
+
+	cmd := internal_exec.LoggedCommand("tune2fs",
+		"-l",
+		devPath)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return 0, err
+	}
+
+	matches := blockCountPatternRe.FindSubmatch(output)
+	if len(matches) < 2 {
+		return 0, fmt.Errorf("unable to determine block size for filesystem on %s", devPath)
+	}
+
+	blockCount, err := strconv.Atoi(string(matches[1]))
+	if err != nil {
+		return 0, err
+	}
+
+	return uint64(blockCount), nil
+}
